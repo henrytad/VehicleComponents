b5156045f18e8cc9068004d7ec8c444cfaabb397
Henry Tadeusiak <htadeusiak1@gmail.com>
2026-09-19 19:24:40 -0400

    fix: remove include

--- src/vehicle.jl
+++ src/vehicle.jl
@@ -371,7 +371,6 @@ function wheelbase(vehicle::Vehicle)
     return front.geometry.linkages.left.wheel_center[1] - rear.geometry.linkages.left.wheel_center[1]
 end
 
-include("vehicles/MR25.jl")
 include("vehicles/TestVehicle.jl")
 
 end # module Data
